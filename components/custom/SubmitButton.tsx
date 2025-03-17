7cdb86a072b59f16643d34419b41157fa7a235c2
demchkoa <demchkoartem05@gmail.com>
2025-03-17 14:34:12 +0200

    disabled submit button

--- components/custom/SubmitButton.tsx
+++ components/custom/SubmitButton.tsx
@@ -6,7 +6,7 @@ import { Loader2 } from "lucide-react";
 export const SubmitButton = () => {
   const { pending } = useFormStatus();
   return (
-    <Button className="w-full">
+    <Button className="w-full" type="submit" disabled={pending}>
       {pending ? (
         <div className="flex items-center gap-2">
           <Loader2 className="w-8 h-8 animate-spin" />
